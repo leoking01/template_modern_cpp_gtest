d059691f7a7369e62454584f13f89cab524e9ad0
Your Name <you@example.com>
2021-06-27 20:12:32 -0700

    init.

---
+++ SrcMain/src/tmp.cpp
@@ -0,0 +1,10 @@
+#include "project/tmp.hpp"
+
+int tmp::add(int a, int b) { return a + b; }
+
+#include  <iostream>
+int main()
+{
+    std::cout<< "hello,a."<< std::endl;
+    return  0 ;
+}
---
+++ install_Project/include/project/tmp.hpp
@@ -0,0 +1,11 @@
+#ifndef TMP_TMP_H_
+#define TMP_TMP_H_
+
+namespace tmp
+{
+
+_declspec(dllexport)
+  int add(int, int);
+}
+
+#endif  // TMP_TMP_H_
